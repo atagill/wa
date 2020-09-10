29798595a32c190a2aa7eb70a8dbd399f817bcb9
atagill <atagill@gmail.com>
2020-09-10 23:34:52 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def _get_parsed_line(input_line, persons_list):
             dirty_timestamp_string = dirty_timestamp_string.replace(remove_character, "")
 
         try:
-            timestamp_string = parse_datetime(dirty_timestamp_string, dayfirst=True)
+            timestamp_string = parse_datetime(dirty_timestamp_string, ignoretz=True, dayfirst=True)
             line = timestamp_splitter.join(items[1:]).strip()
             break
         except ValueError:
@@ -25,7 +25,7 @@ def _get_parsed_line(input_line, persons_list):
     if not timestamp_string:
         raise IndexError
     items = line.split(":")
-    text_string = "AAA:".join(items[1:]).strip()
+    text_string = ":".join(items[1:]).strip()
     if not text_string:
         return None, persons_list
 
